bd869ed841f2375935c3c8f75b3f59ed6d0aa840
Philip Dobranowski <dobranowskip1@gmail.com>
2025-07-19 19:03:02 -0400

    Basic block creation, working for test case

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use std::fs;
 use std::collections::HashMap;
-use lc3_ensemble::ast::{asm::{disassemble_line, AsmInstr, Stmt, StmtKind}, Label, PCOffset};
+use lc3_ensemble::ast::{asm::{disassemble_line, AsmInstr, Stmt, StmtKind}, Label, PCOffset, CondCode};
+
+#[derive(Debug, Clone)]
+struct BasicBlock {
+    address: u16,
+    length: u16,
+    preds: Vec<u16>,
+    succs: Vec<u16>,
+}
+
+impl BasicBlock {
+    fn new(address: u16) -> Self {
+        BasicBlock {
+            address,
+            length: 0,
+            preds: Vec::new(),
+            succs: Vec::new(),
+        }
+    }
+}
 
 fn gen_sections(content: &str) -> (Vec<&str>, HashMap<u16, &str>) {
     let sections: Vec<&str> = content.split("\n\n").collect();
@@ -46,7 +65,7 @@ fn disassemble(text: Vec<&str>, symbols: HashMap<u16, &str>) -> (HashMap<u16, St
     (disassembled, origs)
 }
 
-fn identify_code_data(disassembly: HashMap<u16, Stmt>, origs: Vec<u16>) {
+fn identify_code_data(disassembly: HashMap<u16, Stmt>, origs: Vec<u16>) -> Vec<u16> {
     let mut call_list: Vec<u16> = origs.clone();
     let mut function_list: Vec<u16> = Vec::new();
     let mut work_list: Vec<u16> = Vec::new();
@@ -59,31 +78,38 @@ fn identify_code_data(disassembly: HashMap<u16, Stmt>, origs: Vec<u16>) {
             let stmt = &disassembly[&addr];
 
             match &stmt.nucleus {
-                StmtKind::Instr(AsmInstr::JMP(_reg)) => {
-                    // no good way to handle this for now
-                },
+                // Instruction is jump
                 StmtKind::Instr(AsmInstr::BR(_cc, pcoffset9)) => {
                     let value = match pcoffset9 {
                         PCOffset::Offset(offset) => Some(offset.get()),
                         PCOffset::Label(_) => None,
                     }.unwrap();
-                    work_list.push((addr as i16 + value) as u16);
+                    work_list.push((addr as i16 + 1 + value) as u16);
                 },
-                StmtKind::Instr(AsmInstr::JSRR(_reg)) => {
-                    // no good way to handle this for now
-                },
+                // Instruction is call
                 StmtKind::Instr(AsmInstr::JSR(pcoffset11)) => {
                     let value = match pcoffset11 {
                         PCOffset::Offset(offset) => Some(offset.get()),
                         PCOffset::Label(_) => None,
                     }.unwrap();
-                    call_list.push((addr as i16 + value) as u16);
+                    call_list.push((addr as i16 + 1 + value) as u16);
                 },
+                // Instruction is ret
                 StmtKind::Instr(AsmInstr::RET) => {
                     if work_list.is_empty() {
                         break;
                     }
                 },
+                // Instruction is unconditional jump/call
+                StmtKind::Instr(AsmInstr::JMP(_reg)) => {
+                    addr = work_list.pop().unwrap();
+                    continue;
+                },
+                StmtKind::Instr(AsmInstr::JSRR(_reg)) => {
+                    addr = work_list.pop().unwrap();
+                    continue;
+                },
+                // If halt or directive, guaranteed end of function
                 StmtKind::Instr(AsmInstr::HALT) => {
                     break;
                 },
@@ -91,13 +117,257 @@ fn identify_code_data(disassembly: HashMap<u16, Stmt>, origs: Vec<u16>) {
                     break;
                 },
                 _ => {
-                    // er
+                    // Any other instruction falls through to increment
                 }
             }
 
             addr += 1;
         }
     }
+
+    function_list
+}
+
+fn get_block_at(start_address: u16, block_list: &mut HashMap<u16, BasicBlock>) -> &mut BasicBlock {
+    if !block_list.contains_key(&start_address) {
+        block_list.insert(start_address, BasicBlock::new(start_address));
+    }
+    block_list.get_mut(&start_address).unwrap()
+}
+
+fn find_next_label(current_address: u16, disassembly: &HashMap<u16, Stmt>) -> Option<u16> {
+    let mut addr = current_address + 1;
+    while let Some(stmt) = disassembly.get(&addr) {
+        if !stmt.labels.is_empty() {
+            return Some(addr);
+        }
+        addr += 1;
+    }
+    None
+}
+
+fn find_next_branch(current_address: u16, disassembly: &HashMap<u16, Stmt>) -> Option<(u16, u16, bool, bool)> {
+    let mut addr = current_address;
+    while let Some(stmt) = disassembly.get(&addr) {
+        let is_branch = match &stmt.nucleus {
+            // Jump, branch, return, and halt instructions all end basic blocks
+            // Calls do not end basic blocks
+            StmtKind::Instr(AsmInstr::BR(_, _)) |
+            StmtKind::Instr(AsmInstr::JMP(_)) |
+            StmtKind::Instr(AsmInstr::RET) |
+            StmtKind::Instr(AsmInstr::HALT) => true,
+            _ => false,
+        };
+
+        if is_branch {
+            let destination_known = match &stmt.nucleus {
+                StmtKind::Instr(AsmInstr::BR(_, _)) => true,
+                StmtKind::Instr(AsmInstr::JMP(_)) => false,
+                StmtKind::Instr(AsmInstr::RET) => false,
+                StmtKind::Instr(AsmInstr::HALT) => false,
+                _ => false,
+            };
+            let is_conditional = match &stmt.nucleus {
+                StmtKind::Instr(AsmInstr::BR(cc, _)) => *cc != 7,
+                _ => false,
+            };
+            return Some((addr, addr + 1, destination_known, is_conditional));
+        }
+        addr += 1;
+    }
+    None
+}
+
+fn get_branch_destination(address: u16, disassembly: &HashMap<u16, Stmt>) -> Option<u16> {
+    if let Some(stmt) = disassembly.get(&address) {
+        match &stmt.nucleus {
+            StmtKind::Instr(AsmInstr::BR(_, pcoffset9)) => {
+                if let PCOffset::Offset(offset) = pcoffset9 {
+                    return Some((address as i16 + 1 + offset.get()) as u16);
+                }
+            },
+            _ => {}
+        }
+    }
+    None
+}
+
+fn create_basic_blocks(entry_address: u16, disassembly: &HashMap<u16, Stmt>) -> HashMap<u16, BasicBlock> {
+    let mut block_list: HashMap<u16, BasicBlock> = HashMap::new();
+    let mut work_list: Vec<u16> = Vec::new();
+
+    // Initialize with entry block
+    {
+        let entry_block = get_block_at(entry_address, &mut block_list);
+        entry_block.address = entry_address;
+    }
+    work_list.push(entry_address);
+
+    while let Some(current_address) = work_list.pop() {
+        if block_list.contains_key(&current_address) && block_list[&current_address].length > 0 {
+            continue;
+        }
+
+        let mut current_addr = current_address;
+        let block_start = current_addr;
+
+        loop {
+            let next_label = find_next_label(current_addr, disassembly);
+            let next_branch = find_next_branch(current_addr, disassembly);
+
+            //println!("Current address: {:04X}, Block start: {:04X}", current_addr, block_start);
+            //println!("Next label: {:?}, Next branch: {:?}", next_label, next_branch);
+
+            match (next_label, next_branch) {
+                // Both label and branch found
+                (Some(label_addr), Some((branch_addr, branch_after, destination_known, is_conditional))) => {
+                    // Label is first, block ends at label
+                    if label_addr < branch_after {
+                        {
+                            let block = get_block_at(block_start, &mut block_list);
+                            block.length = label_addr - block_start;
+                        }
+
+                        // Successor relationship
+                        let next_block_addr = label_addr;
+                        get_block_at(next_block_addr, &mut block_list);
+
+                        {
+                            let block = get_block_at(block_start, &mut block_list);
+                            block.succs.push(next_block_addr);
+                        }
+                        {
+                            let next_block = get_block_at(next_block_addr, &mut block_list);
+                            next_block.preds.push(block_start);
+                        }
+
+                        work_list.push(label_addr);
+                        break;
+                    }
+                    // Branch comes first, block ends at branch
+                    else {
+                        {
+                            let block = get_block_at(block_start, &mut block_list);
+                            block.length = branch_after - block_start;
+                        }
+
+                        if !destination_known {
+                            break;
+                        }
+
+                        // Branch destination address
+                        if let Some(dest_addr) = get_branch_destination(branch_addr, disassembly) {
+                            get_block_at(dest_addr, &mut block_list);
+
+                            {
+                                let block = get_block_at(block_start, &mut block_list);
+                                block.succs.push(dest_addr);
+                            }
+                            {
+                                let dest_block = get_block_at(dest_addr, &mut block_list);
+                                dest_block.preds.push(block_start);
+                            }
+
+                            work_list.push(dest_addr);
+                        }
+
+                        if !is_conditional {
+                            break;
+                        }
+
+                        // Fall through for conditional branches
+                        let fall_through_addr = branch_after;
+                        get_block_at(fall_through_addr, &mut block_list);
+
+                        {
+                            let block = get_block_at(block_start, &mut block_list);
+                            block.succs.push(fall_through_addr);
+                        }
+                        {
+                            let fall_through_block = get_block_at(fall_through_addr, &mut block_list);
+                            fall_through_block.preds.push(block_start);
+                        }
+
+                        work_list.push(fall_through_addr);
+                        break;
+                    }
+                },
+                // Only branch found
+                (None, Some((branch_addr, branch_after, destination_known, is_conditional))) => {
+                    {
+                        let block = get_block_at(block_start, &mut block_list);
+                        block.length = branch_after - block_start;
+                    }
+
+                    if !destination_known {
+                        break;
+                    }
+
+                    if let Some(dest_addr) = get_branch_destination(branch_addr, disassembly) {
+                        get_block_at(dest_addr, &mut block_list);
+
+                        {
+                            let block = get_block_at(block_start, &mut block_list);
+                            block.succs.push(dest_addr);
+                        }
+                        {
+                            let dest_block = get_block_at(dest_addr, &mut block_list);
+                            dest_block.preds.push(block_start);
+                        }
+
+                        work_list.push(dest_addr);
+                    }
+
+                    if !is_conditional {
+                        break;
+                    }
+
+                    let fall_through_addr = branch_after;
+                    get_block_at(fall_through_addr, &mut block_list);
+
+                    {
+                        let block = get_block_at(block_start, &mut block_list);
+                        block.succs.push(fall_through_addr);
+                    }
+                    {
+                        let fall_through_block = get_block_at(fall_through_addr, &mut block_list);
+                        fall_through_block.preds.push(block_start);
+                    }
+
+                    work_list.push(fall_through_addr);
+                    break;
+                },
+                // Only label found
+                (Some(label_addr), None) => {
+                    {
+                        let block = get_block_at(block_start, &mut block_list);
+                        block.length = label_addr - block_start;
+                    }
+
+                    let next_block_addr = label_addr;
+                    get_block_at(next_block_addr, &mut block_list);
+
+                    {
+                        let block = get_block_at(block_start, &mut block_list);
+                        block.succs.push(next_block_addr);
+                    }
+                    {
+                        let next_block = get_block_at(next_block_addr, &mut block_list);
+                        next_block.preds.push(block_start);
+                    }
+
+                    work_list.push(label_addr);
+                    break;
+                },
+                // No label or branch found
+                (None, None) => {
+                    break;
+                }
+            }
+        }
+    }
+
+    block_list
 }
     
 fn main() {
@@ -111,15 +381,35 @@ fn main() {
     assert!(input_obj.starts_with("LC-3 OBJ FILE"));
 
     let (text, symbols) = gen_sections(&input_obj);
-
     let (disassembly, origs) = disassemble(text, symbols);
 
+    // Print disassembly for debugging purposes
     let mut sorted_entries: Vec<_> = disassembly.clone().into_iter().collect();
     sorted_entries.sort_by_key(|&(key, _)| key);
-
     for (addr, stmt) in sorted_entries {
         println!("{:04X}: {}", addr, stmt);
     }
 
-    identify_code_data(disassembly, origs);
+    let function_list = identify_code_data(disassembly.clone(), origs);
+
+    // Print identified functions for debugging purposes
+    println!("Identified functions:");
+    for addr in &function_list {
+        println!("{:04X}", addr);
+    }
+    
+    for &entry_addr in &function_list {
+        let blocks = create_basic_blocks(entry_addr, &disassembly);
+
+        // Print basic blocks for debugging purposes
+        let mut sorted_blocks: Vec<_> = blocks.into_iter().collect();
+        sorted_blocks.sort_by_key(|&(addr, _)| addr);
+        for (addr, block) in sorted_blocks {
+            let preds: Vec<String> = block.preds.iter().map(|&p| format!("{:04X}", p)).collect();
+            let succs: Vec<String> = block.succs.iter().map(|&s| format!("{:04X}", s)).collect();
+            println!("Block at {:04X}: length = {}, preds = [{}], succs = [{}]", 
+                     addr, block.length, preds.join(", "), succs.join(", "));
+        }
+    }
 }
+
